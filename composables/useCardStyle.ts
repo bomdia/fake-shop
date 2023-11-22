1da2b52771c8e6645ec84b7d19f4ba17cb6b93c4
Bomdia <bellatranello@gmail.com>
2023-11-22 01:10:47 +0100

    generilizzazione funzione per stile reattivo veloce

--- composables/useCardStyle.ts
+++ composables/useCardStyle.ts
@@ -1,18 +1,33 @@
+import type { CSSProperties } from 'vue'
 import { useDisplay } from 'vuetify/lib/framework.mjs'
+export type ReactiveStyle = Partial<{
+  xs: CSSProperties
+  sm: CSSProperties
+  md: CSSProperties
+  lg: CSSProperties
+  xl: CSSProperties
+}>
 
-export function useSingleFormCardStyle(): Ref<
-  Partial<{ width: string; alignSelf: string }>
-> {
+export function useReactiveStyle(
+  style: MaybeRef<ReactiveStyle>
+): Ref<CSSProperties> {
   const display = useDisplay()
   return computed(() => {
-    if (display.xlAndUp.value) return {}
+    const styleProps = unref(style)
+    if (display.xlAndUp.value && styleProps.xl) return styleProps.xl
+    if (display.lgAndUp.value && styleProps.lg) return styleProps.lg
+    if (display.mdAndUp.value && styleProps.md) return styleProps.md
+    if (display.smAndUp.value && styleProps.sm) return styleProps.sm
+    return styleProps.xs ?? {}
+  })
+}
 
-    if (display.lgAndUp.value) return { width: '60%' }
-
-    if (display.mdAndUp.value) return { width: '80%' }
-
-    if (display.smAndUp.value) return { width: '90%' }
-
-    return { width: '100%', alignSelf: 'baseline' }
+export function useSingleFormCardStyle(): Ref<CSSProperties> {
+  return useReactiveStyle({
+    xs: { width: '100%', alignSelf: 'baseline' },
+    sm: { width: '90%' },
+    md: { width: '80%' },
+    lg: { width: '60%' },
+    xl: { width: '40%' },
   })
 }
